cdd3258761a208e908483769fcd2323f37849679
Bogdan Bujdea <contact@bogdanbujdea.dev>
2022-08-29 23:46:17 +0300

    Added task fetcher service with unit tests

---
+++ tests/TodoistApp.UnitTests/TaskFetcherUnitTtests.cs
@@ -0,0 +1,14 @@
+using TodoistApp.Blazor.Services;
+
+namespace TodoistApp.UnitTests
+{
+    public class TaskFetcherUnitTtests
+    {
+        [Fact]
+        public void GetNumberOfTasks_Should_Return_0()
+        {
+            var numberOfTasks = new TaskFetcher().GetNumberOfTasks();
+            Assert.Equal(0, numberOfTasks);
+        }
+    }
+}
