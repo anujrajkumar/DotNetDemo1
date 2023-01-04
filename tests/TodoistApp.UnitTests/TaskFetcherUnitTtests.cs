3960a2cbfae91baf43412725b3701c6cff53d000
Dev Ops <devops@DAMCOSOFT.onmicrosoft.com>
2023-01-04 14:56:44 +0000

    Updated TaskFetcherUnitTtests.cs

--- tests/TodoistApp.UnitTests/TaskFetcherUnitTtests.cs
+++ tests/TodoistApp.UnitTests/TaskFetcherUnitTtests.cs
@@ -30,18 +30,3 @@ namespace TodoistApp.UnitTests
         
     }
 }
-
-namespace Prime.UnitTests.Services
-{
-    public class PrimeService_IsPrimeShould
-    {
-        [Fact]
-        public void IsPrime_InputIs1_ReturnFalse()
-        {
-            var primeService = new PrimeService();
-            bool result = primeService.IsPrime(1);
-
-            Assert.False(result, "1 should not be prime");
-        }
-    }
-}
